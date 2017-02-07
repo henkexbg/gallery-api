9b62c7511e11bf3a6e142b29fdc9ec202a281a4f
Henrik Bjerne <henrik.bjerne@gmail.com>
2017-02-07 22:14:18 +1100

    Refactored to new approved package com.github.henkexbg

---
+++ src/main/java/com/github/henkexbg/gallery/controller/model/GalleryFileHolder.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2016 Henrik Bjerne
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:The above copyright
+ * notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * 
+ */
+package com.github.henkexbg.gallery.controller.model;
+
+/**
+ * POJO defining one gallery file. This file will be sent back to the requesting
+ * entity (of course with possible transformation to for instance JSON).
+ * 
+ * @author Henrik Bjerne
+ *
+ */
+public class GalleryFileHolder {
+
+    private String freeSizePath;
+    
+    private String formatPath;
+
+    private String contentType;
+
+    private String filename;
+
+    public String getFreeSizePath() {
+        return freeSizePath;
+    }
+
+    public void setFreeSizePath(String freeSizePath) {
+        this.freeSizePath = freeSizePath;
+    }
+
+    public String getFormatPath() {
+        return formatPath;
+    }
+
+    public void setFormatPath(String formatPath) {
+        this.formatPath = formatPath;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+}
