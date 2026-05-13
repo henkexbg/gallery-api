32d8b650dd336bb25680d1d8d4d4232e82e7ba45
henkexbg <henrik.bjerne@gmail.com>
2026-05-13 20:28:23 +1000

    Added waiting for directories in addition to regular files in Deduplicator.

--- src/main/java/com/github/henkexbg/gallery/service/Deduplicator.java
+++ src/main/java/com/github/henkexbg/gallery/service/Deduplicator.java
@@ -4,12 +4,15 @@ import com.github.henkexbg.gallery.util.GalleryFileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
 
 /**
  * Deduplicates file events by receiving events and then withholds emitting them until the filesize has stabilized.
@@ -17,36 +20,53 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Deduplicator {
 
     private final Logger LOG = LoggerFactory.getLogger(getClass());
+    private final Map<Path, FileInfo> trackingMap = new ConcurrentHashMap<>();
+    private final Thread thread;
+    private final List<Listener> listeners = new ArrayList<>();
 
     int intervalMillis = 500;
     int nrStableCyclesRequired = 3;
-    Map<Path, FileSizeCounterPair> trackingMap = new ConcurrentHashMap<>();
-    Thread thread;
+    int nrStableCyclesRequiredForDirectories = 1;
     private volatile boolean running = true;
-    List<Listener> listeners = new ArrayList<>();
 
     public Deduplicator() {
         LOG.debug("Initializing Deduplicator");
-        Thread.ofVirtual().start(() -> {
+        thread = Thread.ofVirtual().start(() -> {
             while (running) {
                 try {
+                    List<Path> stablePaths;
+                    List<Listener> privateListeners;
                     synchronized (this) {
                         wait(intervalMillis);
-                        List<Path> stablePaths = getAndRemoveStablePaths();
+                        stablePaths = removeIncrementAndReturnStablePaths();
+                        privateListeners = new ArrayList<>(listeners);
                         if (stablePaths.isEmpty()) {
                             continue;
                         }
                         LOG.debug("Found {} stable paths. Will notify listeners. There are {} unstable paths waiting to stabilize",
                                 stablePaths.size(), trackingMap.size());
-                        listeners.forEach(listener -> {
-                            try {
-                                LOG.debug("Notifying listener {} of stable paths {}", listener, stablePaths);
-                                listener.onPathsChanged(stablePaths);
-                            } catch (Exception e) {
-                                LOG.error("Exception when notifying listener {}. Will proceed with other listeners", listener, e);
+
+                        // Handle files in newly stabilized directories
+                        List<Path> filesInStableDirs = new ArrayList<>();
+                        stablePaths.stream().filter(p -> p.toFile().isDirectory()).forEach(dir -> {
+                            try (Stream<Path> pathStream = Files.list(dir)) {
+                                pathStream.filter(Files::isRegularFile).forEach(filesInStableDirs::add);
+                            } catch (IOException e) {
+                                LOG.error("Error while listing files in directory {}. Will proceed with other files", dir, e);
                             }
                         });
+                        LOG.debug("Found {} files in newly stabilized directories", filesInStableDirs.size());
+                        filesInStableDirs.forEach(this::add);
                     }
+                    // Notify listeners with all stable paths outside of synchronized block
+                    privateListeners.forEach(listener -> {
+                        try {
+                            LOG.debug("Notifying listener {} of stable paths {}", listener, stablePaths);
+                            listener.onPathsChanged(stablePaths);
+                        } catch (Exception e) {
+                            LOG.error("Exception when notifying listener {}. Will proceed with other listeners", listener, e);
+                        }
+                    });
                 } catch (InterruptedException e) {
                     LOG.debug("Deduplicator thread interrupted");
                 }
@@ -55,27 +75,32 @@ public class Deduplicator {
         });
     }
 
+    /**
+     * Adds a listener that will be notified with paths that have stabilized.
+     *
+     * @param listener Listener
+     * @see Listener
+     */
     public synchronized void addListener(Listener listener) {
         listeners.add(listener);
     }
 
     /**
-     * Adds a path. If it's a directory, listeners are notified immediately. It it's a file, it will be added to an internal structure,
-     * which will emit the changed path once the size of the file is stable.
+     * Adds a path. Listeners will be notified once the underlying file is considered stable. For regular files this means the filesize is
+     * stable over a certain period. Directories are kept for some time as well, and all regular files under that directory will be added
+     * automatically just before the directory is emitted to listeners.
      *
      * @param path Path
      */
     public synchronized void add(Path path) {
-        if (path.toFile().isDirectory()) {
-            notifyListeners(List.of(path));
-            return;
-        }
-        FileSizeCounterPair fileSizeCounterPair = trackingMap.get(path);
-        if (fileSizeCounterPair == null) {
-            trackingMap.put(path, new FileSizeCounterPair(path.toFile().length(), new AtomicInteger(0)));
-        } else {
-            if (fileSizeCounterPair.fileSize() != path.toFile().length()) {
-                fileSizeCounterPair.counter().set(0);
+        LOG.debug("Adding path {} to Deduplicator", path);
+        boolean directory = path.toFile().isDirectory();
+        FileInfo fileInfo = trackingMap.get(path);
+        if (fileInfo == null) {
+            trackingMap.put(path, new FileInfo(path.toFile().length(), new AtomicInteger(0), directory));
+        } else if (!directory) {
+            if (fileInfo.fileSize() != path.toFile().length()) {
+                fileInfo.counter().set(0);
             }
         }
     }
@@ -92,28 +117,26 @@ public class Deduplicator {
         }
     }
 
-    private synchronized void notifyListeners(List<Path> paths) {
-        listeners.forEach(listener -> {
-            try {
-                listener.onPathsChanged(paths);
-            } catch (Exception e) {
-                LOG.error("Exception when notifying listener {}. Will proceed with other listeners", listener, e);
-            }
-        });
-    }
-
-    private synchronized List<Path> getAndRemoveStablePaths() {
-        List<Path> stablePaths =
-                trackingMap.entrySet().stream().filter(e -> e.getValue().counter().get() >= nrStableCyclesRequired).map(Map.Entry::getKey)
-                        .sorted(GalleryFileUtils.shortestPathComparatorPath()).toList();
+    /**
+     * Removes all paths that have reached the stable state and returns them. Also increments the counter for all remaining paths.
+     * @return A list of paths that have reached the stable state
+     */
+    private synchronized List<Path> removeIncrementAndReturnStablePaths() {
+        List<Path> stablePaths = trackingMap.entrySet().stream()
+                .filter(e -> (e.getValue().directory() && e.getValue().counter().get() >= nrStableCyclesRequiredForDirectories) ||
+                        e.getValue().counter().get() >= nrStableCyclesRequired).map(Map.Entry::getKey)
+                .sorted(GalleryFileUtils.shortestPathComparatorPath()).toList();
         stablePaths.forEach(trackingMap::remove);
         trackingMap.values().forEach(v -> v.counter().incrementAndGet());
         return stablePaths;
     }
 
-    record FileSizeCounterPair(long fileSize, AtomicInteger counter) {
+    record FileInfo(long fileSize, AtomicInteger counter, boolean directory) {
     }
 
+    /**
+     * Listener that will be notified with paths that have stabilized.
+     */
     @FunctionalInterface
     public interface Listener {
         void onPathsChanged(List<Path> paths);
